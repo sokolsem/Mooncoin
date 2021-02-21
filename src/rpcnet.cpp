32786a456ee191801700aa96e8cd8948a598de42
Semen Sokolov <34645389+sokolsem@users.noreply.github.com>
2021-02-21 16:58:32 +0300

    Update rpcnet.cpp

--- src/rpcnet.cpp
+++ src/rpcnet.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2009-2012 Bitcoin Developers
 // Copyright (c) 2013-2014 Dogecoin Developers
 // Copyright (c) 2013-2014 Team Mooncoin
+// Copyright (c) 2011-2021 WEA Developer
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
